643c18f74536d3ae7e5d54e8842375302219db55
Pham <stevenpham1996@outlook.com>
2024-06-17 17:51:11 +0700

    Alter module name to 'cattery'

--- models/kitten.py
+++ models/kitten.py
@@ -4,61 +4,65 @@ from dateutil.relativedelta import relativedelta
 
 
 class Kitten(models.Model):
-    _name = 'cat_cattery.kitten'
-    _description = 'Kittens'
-    
+    _name = "cattery.kitten"
+    _description = "Kittens"
+
     # --------------------------------------- Default Methods ----------------------------------
     def _default_birth_date(self):
-        return (fields.Date.today() - relativedelta(weeks=8))
-    
-    active = fields.Boolean("Active", default=True) 
+        return fields.Date.today() - relativedelta(weeks=8)
+
+    active = fields.Boolean("Active", default=True)
     name = fields.Char("Name")
-    gender = fields.Selection(selection=[('male', 'Male'), ('female', 'Female')], string="Gender",  required=True)
+    gender = fields.Selection(
+        selection=[("male", "Male"), ("female", "Female")],
+        string="Gender",
+        required=True,
+    )
     birth_date = fields.Date(
-        "Birth Date", 
-        default=lambda self: self._default_birth_date(), # Kitten must be at least 8 weeks old 
-        required=True)
+        "Birth Date",
+        default=lambda self: self._default_birth_date(),  # Kitten must be at least 8 weeks old
+        required=True,
+    )
     color = fields.Char("Color")
     temperament = fields.Char("Temperament")
     state = fields.Selection(
         selection=[
-            ('not_available', "Not Arrived"),
+            ("not_available", "Not Arrived"),
             ("available", "Available"),
-            ("adopted", "Adopted"), 
-        ],           
+            ("adopted", "Adopted"),
+        ],
         string="Status",
         required=True,
         copy=False,
-        default="available"
+        default="available",
     )
     image = fields.Binary("Kitten's Image")
-    
+
     # --------------------------------------- Relational Fields ----------------------------------
-    breed_id = fields.Many2one("cat_cattery.breed", string="Breed")
+    breed_id = fields.Many2one("cattery.breed", string="Breed")
     adopter_id = fields.Many2one("res.partner", string="Adopter")
     # --------------------------------------- Computed Fields ----------------------------------
     age = fields.Integer(string="Weeks Old", compute="_compute_age")
-    
+
     @api.depends("birth_date")
     def _compute_age(self):
         for record in self:
             record.age = round((fields.Date.today() - record.birth_date).days / 7)
-
 
     # --------------------------------------- Action Methods ----------------------------------
     def action_adopt(self):
         if self.state == "adopted":
             raise UserError("Sorry, the kitten has gotten a new home.")
         elif self.state == "not_available":
-            raise UserError("The kitten's not yet arrived at the cattery,\
-                but we've registered your interest, please wait.")
+            raise UserError(
+                "The kitten's not yet arrived at the cattery,\
+                but we've registered your interest, please wait."
+            )
         return self.write({"state": "adopted"})
-     
+
     # --------------------------------------- Constraints ----------------------------------
     @api.constrains("state", "adopter_id")
     def _check_adopter_id(self):
         for record in self:
             if record.state != "adopted" and record.adopter_id:
                 raise ValidationError("Only adopted kittens can have an adopter.")
-    
-                
--- report/cattery_adopter_report.py
+++ report/cattery_adopter_report.py
@@ -1,19 +1,19 @@
 from odoo import api, models
 
+
 class AdopterReport(models.AbstractModel):
-    _name = "report.cat_cattery.adopter_report"
-    
+    _name = "report.cattery.adopter_report"
+
     @api.model
     def _get_report_values(self, docids, data=None):
         domain = [("adopter_id", "in", docids)]
-        kittens = self.env["cat_cattery.kitten"].search(domain)
+        kittens = self.env["cattery.kitten"].search(domain)
         adopters = kittens.mapped("adopter_id")
         adopter_kittens = [
-                    (adopter,
-                    kittens.filtered(lambda kitten: kitten.adopter_id == adopter))
-                    for adopter in adopters
-                    ]
+            (adopter, kittens.filtered(lambda kitten: kitten.adopter_id == adopter))
+            for adopter in adopters
+        ]
         docargs = {
-        "adopter_kittens": adopter_kittens,
+            "adopter_kittens": adopter_kittens,
         }
         return docargs
